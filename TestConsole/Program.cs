b9a439d37f31a6fad306136fbc78521f9ea75837
Saif Alam Khan <168960294+saifalamkhan123@users.noreply.github.com>
2024-09-26 13:05:12 +0500

    Re-adding project files

---
+++ TestConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Testing Console");
+
+            Console.WriteLine("================================");
+
+            Console.WriteLine("  ");
+
+
+            List<Employee> list = new List<Employee>
+            { 
+
+                new Employee{Id = 10, Name = "Fahad Iqbal", Address="Nazimabad"},
+                new Employee{Id = 12, Name = "Yaseen", Address="D.H.A"},
+                new Employee{Id = 14, Name = "Waqar Alam", Address="Saddar"},
+                new Employee{Id = 16, Name = "Jamal Ahmed", Address="Korangi"}
+
+            };
+
+
+            foreach (Employee emp in list)
+            {
+
+                Console.WriteLine(emp.Id + " " + emp.Name + " " + emp.Address);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
